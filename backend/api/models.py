17f72f1d37cac54684f857c52e0b82ebece23f47
Bejenaru Victor <bejenaru0victor@gmail.com>
2024-05-03 13:22:43 +0300

    db schema dev

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime 
 
-# Create your models here.
+
+class Course(models.Model):
+    author = models.ForeignKey("users.User", verbose_name=_(""), on_delete=models.SET_NULL)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=1000, null=False, blank=False)
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    video = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=20000, null=False, blank=False)
+
+
+class Subscriber_Record(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    progress = models.ManyToManyField(Lesson)
+    active = models.BooleanField(null=False, blank=True, default=True)
+
+
+class Payments(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    ammount = models.FloatField(null=False, blank=False)
+    date = models.DateTimeField(null=False, blank=False, default=datetime.now)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL)
