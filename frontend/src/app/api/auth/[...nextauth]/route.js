b3ec001cccdce642ce490a9859110c9671049f33
Bejenaru Victor <bejenaru0victor@gmail.com>
2024-05-31 07:32:07 +0300

    from axios to fetch

--- frontend/src/app/api/auth/[...nextauth]/route.js
+++ frontend/src/app/api/auth/[...nextauth]/route.js
@@ -35,36 +35,24 @@ export const authOptions = {
                     }
                 }
 
-                // const res = await fetch(process.env.API_ROOT + url, {
-                //     method: "POST",
-                //     headers: {
-                //         "Content-Type": "application/json",
-                //     },
-                //     body: JSON.stringify(body),
-                // })
+                const res = await fetch(process.env.API_ROOT + url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(body),
+                })
 
-                try {
-                    const response = await axios.post(process.env.API_ROOT + url, body, {
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    });
-                
-                    const user = response.data;
-                    console.log("this is the nextauth route", user);
-                    //const user = await res.json()
-                    if ((response.status == 200 && user) || (user?.access)) {
-                        // Any object returned will be saved in `user` property of the JWT
-                        return user
-                    } else {
-                        // If you return null then an error will be displayed advising the user to check their details.
-                        if (credentials?.email && credentials?.password2)
-                            throw new Error(JSON.stringify(user))
-                        return null
-                        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-                    }
-                } catch (error) {
-                    console.error("Error making POST request", error);
+                const user = await res.json()
+                if ((res.status == 200 && user) || (user?.access)) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    if (credentials?.email && credentials?.password2)
+                        throw new Error(JSON.stringify(user))
+                    return null
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
                 }
             }
         })
