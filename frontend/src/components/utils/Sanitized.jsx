3aa04a4a2d32f491b6bda37f339e2627b381b0c9
Bejenaru Victor <bejenaru0victor@gmail.com>
2024-06-19 10:16:35 +0300

    debug

--- frontend/src/components/utils/Sanitized.jsx
+++ frontend/src/components/utils/Sanitized.jsx
@@ -9,11 +9,11 @@ import DOMPurify from 'isomorphic-dompurify'
 export default function Sanitized({value}) {
     const sanitizedHTML = DOMPurify.sanitize(value);
 
-    useEffect(() => {
-        document.querySelectorAll('code').forEach((block) => {
-          hljs.highlightElement(block);
-        })
-      }, [value])
+    // useEffect(() => {
+    //     document.querySelectorAll('code').forEach((block) => {
+    //       hljs.highlightElement(block);
+    //     })
+    //   }, [value])
 
     return <div
         dangerouslySetInnerHTML={{ __html: sanitizedHTML }}
