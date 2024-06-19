4000562acd9f0e0fc700a4fac1eac59027c3c74d
Bejenaru Victor <bejenaru0victor@gmail.com>
2024-06-19 09:38:54 +0300

    dompurify fix

--- frontend/src/components/utils/Sanitized.jsx
+++ frontend/src/components/utils/Sanitized.jsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/github.css'
 
-import DOMPurify from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify'
 
 export default function Sanitized({value}) {
     const sanitizedHTML = DOMPurify.sanitize(value);
