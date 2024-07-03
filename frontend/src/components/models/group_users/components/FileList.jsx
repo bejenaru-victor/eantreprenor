8020618f5ca69cec7b9477e123820f3da212dac6
Bejenaru Victor <bejenaru0victor@gmail.com>
2024-07-03 12:20:00 +0300

    iterator fix

--- frontend/src/components/models/group_users/components/FileList.jsx
+++ frontend/src/components/models/group_users/components/FileList.jsx
@@ -28,7 +28,7 @@ export default function FileList({files}) {
         <div className="my-3 bg-gray-700 w-12 h-[0.2rem] rounded-full"></div>
         <div className="grid max-w-screen-xl mx-auto lg:grid-cols-12 gap-5 mt-7 mb-10">
             {files.map((file) => 
-                <div className="col-span-4 flex flex-row border-2 border-slate-300 hover:border-slate-400 transition-colors shadow-lg p-5 rounded-xl cursor-pointer">
+                <div key={file.file} className="col-span-4 flex flex-row border-2 border-slate-300 hover:border-slate-400 transition-colors shadow-lg p-5 rounded-xl cursor-pointer">
                     <div className="basis-full p-3 overflow-y-hidden">
                     {/*fileNameUrl(file.file)*/}
                         <LightTooltip title={fileNameUrl(file.file)} placement="bottom">
