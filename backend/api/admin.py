0de195a408cf17667f46fd02aa06f71cf4b611f9
Bejenaru Victor <bejenaru0victor@gmail.com>
2024-07-15 13:58:34 +0300

    Purchase course dev

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Course, Lesson, SharedFile
+from .models import Course, Lesson, SharedFile, Purchase
 
 admin.site.register(Course)
 admin.site.register(Lesson)
 admin.site.register(SharedFile)
+admin.site.register(Purchase)
---
+++ frontend/src/components/pages/LessonView.jsx
@@ -0,0 +1,51 @@
+import Link from 'next/link'
+import { getYouTubeVideoId } from "@/utils/fn"
+import Sanitized from '@/components/utils/Sanitized'
+
+
+export default function LessonView({lesson, next_prev}) {
+
+    const next_id = next_prev.data.next
+    const prev_id = next_prev.data.previous
+
+    return <>
+        <div className='w-full flex flex-col flex-1'>
+            <div className="grid lg:gap-8 xl:gap-16 lg:grid-cols-12 flex-1 px-10">
+                <div className="col-span-7 pt-10">
+                    <iframe
+                        className="aspect-video w-full shadow-lg rounded-lg"
+                        src={`https://www.youtube.com/embed/${getYouTubeVideoId(lesson.video_link)}`}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="Embedded YouTube Video"
+                    ></iframe>
+                </div>
+                <div className="col-span-5 relative">
+                    <div className="absolute w-full h-full overflow-scroll py-10 px-4">
+                        <h3 className='text-xl font-medium'>Transcript</h3>
+                        <div className="my-6 bg-gray-700 w-12 h-[0.2rem] rounded-full"></div>
+                        <Sanitized value={lesson.description} />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="bg-gray-400 w-full h-[0.1rem] rounded-full"></div>
+        <div className='p-5 flex justify-between'>
+            {prev_id ?
+                <Link href={`/lesson/${prev_id}`}>
+                    <div className='bg-slate-800 font-semibold text-gray-50 cursor-pointer hover:bg-slate-900 hover:text-white hover:shadow-md transition-all px-5 py-3 rounded-full'>
+                        Previous Lesson
+                    </div>
+                </Link> :
+            <div></div>
+            }
+            {next_id &&
+                <Link href={`/lesson/${next_id}`}>
+                    <div className='bg-slate-800 font-semibold text-gray-50 cursor-pointer hover:bg-slate-900 hover:text-white hover:shadow-md transition-all px-5 py-3 rounded-full'>
+                        Next Lesson
+                    </div>
+                </Link>
+            }
+        </div>
+    </>
+}
